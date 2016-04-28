f71a7608be66687b0958fe221e2914567b1982dd
Yang SHEN <yang.shen@commscope.com>
2016-04-28 17:18:42 +0800

    receive intent info.

--- androidlab/src/trainingcourse/AndroidAppTraining01/app/src/main/java/com/example/ys1003/androidapptraining01/DisplayMessageActivity.java
+++ androidlab/src/trainingcourse/AndroidAppTraining01/app/src/main/java/com/example/ys1003/androidapptraining01/DisplayMessageActivity.java
@@ -1,5 +1,7 @@
 package com.example.ys1003.androidapptraining01;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +9,12 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();//来自源 Activity 调用 startActivity(intent); 时的传入
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
+        //setContentView(R.layout.activity_display_message);
+    }
 }
