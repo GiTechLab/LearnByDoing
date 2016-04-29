14931b9cc3b1d28c94aef7bd3ec989231b90b3bf
Yang SHEN <yang.shen@commscope.com>
2016-04-29 16:05:11 +0800

    complete lesson one: Simple Activity & Intent.

--- androidlab/src/trainingcourse/AndroidAppTraining01/app/src/main/java/com/example/ys1003/androidapptraining01/DisplayMessageActivity.java
+++ androidlab/src/trainingcourse/AndroidAppTraining01/app/src/main/java/com/example/ys1003/androidapptraining01/DisplayMessageActivity.java
@@ -1,20 +1,26 @@
 package com.example.ys1003.androidapptraining01;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
 
-/**
- * Created by YS1003 on 4/28/2016.
- */
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    /*
+    * 注意：在此界面上点击左上角的返回按钮与点击左下角的系统自带的返回按钮虽然都返回
+    * MainActivity, 但呈现的结果和背后的原理是不一样的.
+    * */
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        //设置界面的内容要在设定页面布局之后
         Intent intent = getIntent();//来自源 Activity 调用 startActivity(intent); 时的传入
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        
-        //setContentView(R.layout.activity_display_message);
+        TextView textView = (TextView)findViewById(R.id.label_message);
+        textView.setText(message);
     }
 }
--- androidlab/src/trainingcourse/AndroidAppTraining01/app/src/main/java/com/example/ys1003/androidapptraining01/MainActivity.java
+++ androidlab/src/trainingcourse/AndroidAppTraining01/app/src/main/java/com/example/ys1003/androidapptraining01/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.ys1003.androidapptraining01.MESSAGE";
@@ -18,13 +19,22 @@ public class MainActivity extends AppCompatActivity {
         /*
             Intent是在不同组件中(比如两个Activity)提供运行时绑定的对象。
             Intent代表一个应用"想去做什么事"，你可以用它做各种各样的任务，不过大部分的时候他们被用来启动另一个Activity。
+            Intent可以携带称作 extras 的键-值对数据类型
 
              Intent构造函数中有两个参数：
              第一个参数是Context(之所以用this是因为当前Activity是Context的子类)
              第二个参数接受系统发送Intent的应用组件的Class（在这代码中，指将要被启动的 Activity）
          */
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        if(message.length() == 0) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                          "Please input the message.",
+                          Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         //通过intent传递数据时，可以通过 putExtra 方法放要传递的数据
         intent.putExtra(EXTRA_MESSAGE, editText.getText().toString());
         //调用基类的 startActivity 并传入intent 来打开 intent 指定的目标 Activity.
