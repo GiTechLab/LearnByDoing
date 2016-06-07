cd098561120714480cc28f733b389c15e2fc20f0
Yang SHEN <yang.shen@commscope.com>
2016-06-07 17:08:12 +0800

    async\await support for .net 4.0

---
+++ asynclab.net/AsyncLab.N4.0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncLab.N4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowMeAsync();
+
+            //http://www.nuget.org/packages/Microsoft.Bcl.Async
+            //http://blog.csdn.net/qiujuer/article/details/38511821
+            //Nuget: Microsoft.Bcl.Async
+            Console.WriteLine(@"Demo to use async\await feature in .NET 4.0");
+            Console.WriteLine("See, the main process is not blocked by the async method ShowMeAsync..");
+            Console.WriteLine("Main Process Ends.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// See, the main process is not blocked by the async method
+        /// </summary>
+        private static async void ShowMeAsync()
+        {
+            Console.WriteLine(await Task.Factory.StartNew(() => {
+                System.Threading.Thread.Sleep(3000);
+                return "[Show me in Async Way..], I am after the main process message.";
+            }));
+        }
+    }
+}
--- asynclab.net/AsyncLab/Program.cs
+++ asynclab.net/AsyncLab/Program.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace AsyncLab
 {
+    //async的作用是异步执行，await的作用是等待执行结果（会卡住异步方法中await以下的代码，但不会卡死主线程）。
+
+    //http://www.cnblogs.com/yeagen/archive/2013/09/22/3334059.html
     //http://www.cnblogs.com/x-xk/archive/2013/06/05/3118005.html
+    //http://www.dozer.cc/2012/03/async-and-await-in-web-application.html
     //https://msdn.microsoft.com/magazine/jj991977 Async Best Practice
     //https://msdn.microsoft.com/magazine/dn802603.aspx  Async in ASP.NET
     //https://msdn.microsoft.com/zh-cn/magazine/mt149362?author=Stephen%20Cleary
@@ -32,11 +36,23 @@ namespace AsyncLab
             //Option 2: result in depends on the sleep time
             //System.Threading.Thread.Sleep(2000);//Adjust the sleep time can affect the output range...
 
-
+            ShowMeAsync();
+            Console.WriteLine("See, the main process is not blocked by the async method ShowMeAsync..");
             Console.WriteLine("Main Process Ends.");
             //注：是否注释掉ReadKey对显示结果有一定的影响，因为不注释的话，所有线程结果都会显示出来(因为主线程一直没有结束)
             //这个例子说明在Windows Form 类型的程序中，通常UI 主线程不会马上结束，所以这个控制台程序发生的例子不容易在UI程序中遇到
-            //Console.ReadKey();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// See, the main process is not blocked by the async method
+        /// </summary>
+        private static async void ShowMeAsync()
+        {
+            Console.WriteLine(await Task.Factory.StartNew(() => {
+                System.Threading.Thread.Sleep(3000);
+                return "[Show me in Async Way..], I am after the main process message. In fact, I am in another thread.";
+            }));
         }
 
         static async Task DoWork(string taskName)
