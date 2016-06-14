d05e414eca2e7d8c8af54c9f2799dcfb42fff3f2
Yang SHEN <yang.shen@commscope.com>
2016-06-14 17:03:45 +0800

    Initialized Code.

---
+++ attributelab.net/AttributeLab/AttributeLab/CareExceptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeLab.AOP
+{
+    //https://social.msdn.microsoft.com/Forums/en-US/7949d8ba-aab9-4045-9027-92c1eae13227/how-to-catchthe-exception-using-attribute-class-in-c-class?forum=csharplanguage
+    //http://kb.cnblogs.com/page/94983/
+    //http://www.cnblogs.com/ml-virus/p/4127003.html
+    //http://blog.csdn.net/jiujiu28/article/details/43602565
+    //http://wayfarer.cnblogs.com/wayfarer/articles/256909.html
+
+    /*
+     * 常见思路是利用 Proxy 模式，在生成实例前后做AOP处理
+     */
+
+    [AttributeUsage(AttributeTargets.Method)]
+    class CareExceptionAttribute:Attribute
+    {
+
+    }
+}
---
+++ attributelab.net/AttributeLab/AttributeLab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AttributeLab.AOP;
+
+namespace AttributeLab
+{
+    //http://www.cnblogs.com/dudu/articles/4449.html
+    //https://msdn.microsoft.com/en-us/library/aa288454(v=vs.71).aspx
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Attribute Lab...");
+
+            Console.WriteLine("Completed");
+            Console.ReadKey();
+        }
+
+        [CareException]
+        static void TestException()
+        {
+            throw new Exception("This is a Test Exception for attribute CareException.");
+        }
+    }
+}
