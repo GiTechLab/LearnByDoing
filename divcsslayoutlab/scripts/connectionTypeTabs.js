3e3638872e7b6677570686515ff09971d1cbf23f
Yang SHEN <yang.shen@commscope.com>
2016-08-03 16:55:50 +0800

    div + css for web page layout.

---
+++ divcsslayoutlab/scripts/connectionTypeTabs.js
@@ -0,0 +1,20 @@
+﻿{
+  "ConnectionTypeTabs": [
+    {      
+        "TabID": "mpo12Tab",
+        "Title": "MPO12-Connetion Type",
+        "Description": "MPO12-Connetion Type",
+        
+        "OrderIndex": 0,
+        "Visible": true      
+    },
+    {
+        "TabID": "mpo24Tab",
+        "Title": "MPO24-Connetion Type",
+        "Description": "MPO24-Connetion Type",
+        
+        "OrderIndex": 1,
+        "Visible": true      
+    }
+  ] 
+}
