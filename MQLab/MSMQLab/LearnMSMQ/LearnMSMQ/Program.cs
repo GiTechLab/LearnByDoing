e759f62315fc2f6833e3d11ccd99299b3d8f0fb1
Yang SHEN <bizbetter@outlook.com>
2016-10-30 23:12:47 +0800

    MSMQ hand on lab

---
+++ MQLab/MSMQLab/LearnMSMQ/LearnMSMQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Messaging;
+
+//http://www.cnblogs.com/hananbaum/archive/2009/01/26/1380997.html
+
+namespace LearnMSMQSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageQueue mq = new MessageQueue();
+
+            Message message = new Message();
+            //message.Body
+
+        }
+    }
+}
---
+++ MQLab/MSMQLab/LearnMSMQ/LearnMSMQReceiver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Messaging;
+
+namespace LearnMSMQReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageQueue mq = new MessageQueue();
+
+        }
+    }
+}
